8c2628baf726fadff2de5fb60dbd397920105067
Neil de Carteret <n3dst4@gmail.com>
2012-04-04 19:25:55 +0100

    added crude floors and ceilings

--- js/MapScene.js
+++ js/MapScene.js
@@ -5,6 +5,22 @@
     MAZE.MapScene = function (map, scale, material) {
         
         var scene = this.scene = new THREE.Scene(),
+            wallMaterial = new THREE.MeshPhongMaterial({
+                reflectivity: 0.1,
+                refractionRatio: 0.5,
+                color: 0xffffff,
+                vertexColors: true,
+                map: THREE.ImageUtils.loadTexture('proto_blue.png', {}, function() {})
+            }),
+            floorMaterial = new THREE.MeshLambertMaterial({
+                color: 0xffffff,
+                map: THREE.ImageUtils.loadTexture('proto_floor.png', {}, function() {})
+            }),
+            ceilingMaterial = new THREE.MeshLambertMaterial({
+                color: 0xffffff,
+                map: THREE.ImageUtils.loadTexture('proto_white.png', {}, function() {})
+            }),
+            
             x, y, cube;
 
         this.map = map;
@@ -12,16 +28,36 @@
             
         for (y=0; y < map.rows.length; y++) {
             for (x=0; x < map.rows[y].length; x++) {
-                if (!map.rows[y][x].isBlocked) continue;
-                cube = new THREE.Mesh(
-                    new THREE.CubeGeometry( scale, scale, scale, 2, 2, 2),
-                    material);
-                cube.position.x = (x * scale) + (scale/2);
-                cube.position.y = (y * scale) + (scale/2);
-                cube.position.z = (scale/2);
-                scene.add(cube);
+                if (! map.rows[y][x].isBlocked) {
+                    cube = new THREE.Mesh(
+                        new THREE.CubeGeometry( scale, scale, scale, 2, 2, 2),
+                        ceilingMaterial);
+                    cube.position.x = (x * scale) + (scale/2);
+                    cube.position.y = (y * scale) + (scale/2);
+                    cube.position.z = scale + (scale/2);
+                    scene.add(cube);                    
+                    cube = new THREE.Mesh(
+                        new THREE.CubeGeometry( scale, scale, scale, 2, 2, 2),
+                        floorMaterial);
+                    cube.position.x = (x * scale) + (scale/2);
+                    cube.position.y = (y * scale) + (scale/2);
+                    cube.position.z = (-scale/2);
+                    scene.add(cube);                    
+                    
+                }
+                else {
+                    cube = new THREE.Mesh(
+                        new THREE.CubeGeometry( scale, scale, scale, 2, 2, 2),
+                        wallMaterial);
+                    cube.position.x = (x * scale) + (scale/2);
+                    cube.position.y = (y * scale) + (scale/2);
+                    cube.position.z = (scale/2);
+                    scene.add(cube);                    
+                }
             }
         }
+        
+        
         
     }
     
