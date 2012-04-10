8d748848f64aed3f4cef3da1924d29b094be3ab2
Neil de Carteret <n3dst4@gmail.com>
2012-04-10 19:50:58 +0100

    use merged geometry

--- src/MAZE/MapScene.js
+++ src/MAZE/MapScene.js
@@ -4,6 +4,8 @@
     
     MAZE.MapScene = function (map, scale, material) {
         
+        var mesh, cube;
+        
         var scene = this.scene = new THREE.Scene(),
             wallMaterial = new THREE.MeshPhongMaterial({
                 reflectivity: 0.1,
@@ -24,38 +26,60 @@
             x, y, cube;
 
         this.map = map;
+        var floorMaterials = [ floorMaterial, floorMaterial, floorMaterial,
+                             floorMaterial, floorMaterial, floorMaterial ];
+        var wallMaterials = [ wallMaterial, wallMaterial, wallMaterial,
+                             wallMaterial, wallMaterial, wallMaterial];
+        var ceilingMaterials = [ ceilingMaterial, ceilingMaterial, ceilingMaterial,
+                               ceilingMaterial, ceilingMaterial, ceilingMaterial];
+        mergedGeometry = new THREE.Geometry();
 
             
         for (y=0; y < map.rows.length; y++) {
             for (x=0; x < map.rows[y].length; x++) {
                 if (! map.rows[y][x].isBlocked) {
                     cube = new THREE.Mesh(
-                        new THREE.CubeGeometry( scale, scale, scale, 2, 2, 2),
-                        ceilingMaterial);
+                        new THREE.CubeGeometry( scale, scale, scale, 2, 2, 2, ceilingMaterials));
                     cube.position.x = (x * scale) + (scale/2);
                     cube.position.y = (y * scale) + (scale/2);
                     cube.position.z = scale + (scale/2);
-                    scene.add(cube);                    
+                    //scene.add(cube);
+                    THREE.GeometryUtils.merge(mergedGeometry, cube);
                     cube = new THREE.Mesh(
-                        new THREE.CubeGeometry( scale, scale, scale, 2, 2, 2),
+                        new THREE.CubeGeometry( scale, scale, scale, 2, 2, 2, floorMaterials),
                         floorMaterial);
                     cube.position.x = (x * scale) + (scale/2);
                     cube.position.y = (y * scale) + (scale/2);
                     cube.position.z = (-scale/2);
-                    scene.add(cube);                    
+                    //scene.add(cube);
+                    THREE.GeometryUtils.merge(mergedGeometry, cube);
                     
                 }
                 else {
                     cube = new THREE.Mesh(
-                        new THREE.CubeGeometry( scale, scale, scale, 2, 2, 2),
+                        new THREE.CubeGeometry( scale, scale, scale, 2, 2, 2, wallMaterials),
                         wallMaterial);
                     cube.position.x = (x * scale) + (scale/2);
                     cube.position.y = (y * scale) + (scale/2);
                     cube.position.z = (scale/2);
-                    scene.add(cube);                    
+                    THREE.GeometryUtils.merge(mergedGeometry, cube);
+                    //scene.add(cube);                    
                 }
             }
         }
+        mesh = new THREE.Mesh(
+            mergedGeometry,
+            new THREE.MeshFaceMaterial()
+        );
+        //mesh.position.x = 0;
+        //mesh.position.y = 0;
+        //mesh.position.z = 0;
+        mesh.frustumCulled = false;
+        //mesh.matrixAutoUpdate = false;
+        //mesh.geometry.computeFaceNormals();
+        //mesh.geometry.computeVertexNormals();
+        //mesh.updateMatrix();
+        scene.add(mesh);
         
         
         
